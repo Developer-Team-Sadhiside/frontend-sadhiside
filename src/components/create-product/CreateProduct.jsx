43b85699d1a56d9eb18d166fcda5b905c670400b
Bagus Suprapta <69630915+slickeel@users.noreply.github.com>
2022-06-20 18:20:17 +0800

    fix: preview image and button submit

--- src/components/create-product/CreateProduct.jsx
+++ src/components/create-product/CreateProduct.jsx
@@ -3,6 +3,7 @@ import { useRef, useState, useEffect } from 'react';
 
 export default function CreateProduct() {
   const [price, setPrice] = useState();
+
   const [image, setImage] = useState();
   const [preview, setPreview] = useState();
   const fileInputRef = useRef();
@@ -10,11 +11,10 @@ export default function CreateProduct() {
   useEffect(() => {
     if (image) {
       const reader = new FileReader();
-      console.log(reader);
-      reader.onloadend = () => {
+      reader.readAsDataURL(image);
+      reader.onload = function (e) {
         setPreview(reader.result);
       };
-      //reader.readAsDataURL();
     } else {
       setPreview(null);
     }
@@ -26,62 +26,89 @@ export default function CreateProduct() {
   }
 
   return (
-    <div className='create-product-container'>
-      <form className='create-product-form'>
-        <label className='create-product-label'>Nama Produk</label>
-        <input type='text' className='form-control' placeholder='Nama Produk' />
-        <label className='create-product-label'>Harga Produk</label>
-        <input
-          className='form-control'
-          placeholder={`Rp 0.00`}
-          value={price}
-          onChange={(event) => {
-            restrictAlphabets(event.target.value);
-          }}
-        />
-        <label className='create-product-label'>Kategori</label>
-        <select class='form-control'>
-          <option>Hobi</option>
-          <option>Kendaraan</option>
-          <option>Baju</option>
-          <option>Elektronik</option>
-          <option>Kesehatan</option>
-        </select>
-        <label className='create-product-label'>Deskripsi</label>
-        <textarea
-          type='text'
-          className='form-control description'
-          placeholder='Contoh: Jalan Ikan Hiu 33'
-        ></textarea>
+    <div className='create-product-container container'>
+      <div className='row justify-content-sm-center'>
+        <form className='create-product-form col-md-6'>
+          <label className='create-product-label'>Nama Produk</label>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Nama Produk'
+          />
+          <label className='create-product-label'>Harga Produk</label>
+          <input
+            className='form-control'
+            placeholder={`Rp 0.00`}
+            value={price}
+            onChange={(event) => {
+              restrictAlphabets(event.target.value);
+            }}
+          />
+          <label className='create-product-label'>Kategori</label>
+          <select class='form-control'>
+            <option>Hobi</option>
+            <option>Kendaraan</option>
+            <option>Baju</option>
+            <option>Elektronik</option>
+            <option>Kesehatan</option>
+          </select>
+          <label className='create-product-label'>Deskripsi</label>
+          <textarea
+            type='text'
+            className='form-control description'
+            placeholder='Contoh: Jalan Ikan Hiu 33'
+          ></textarea>
 
-        <label className='create-product-label'>Foto Produk</label>
+          <label className='create-product-label'>Foto Produk</label>
+          <div
+            className='picture'
+            onClick={() => {
+              fileInputRef.current.click();
+            }}
+          >
+            {preview ? (
+              <img src={preview} alt='' className='image-uploaded' />
+            ) : (
+              <img src='/svg/fi_plus.svg' alt='' className='plus-svg' />
+            )}
+          </div>
 
-        <div
-          className='picture'
-          onClick={(event) => {
-            fileInputRef.current.click();
-          }}
-        >
-          {/* <img src='/svg/fi_plus.svg' alt='' className='plus-svg' /> */}
-          <img src="/images/secondhand.png" alt="" className='testtt'/>
-        </div>
+          <input
+            type='file'
+            className='form-control'
+            style={{ display: 'none' }}
+            ref={fileInputRef}
+            accept='image/*'
+            onChange={(event) => {
+              const file = event.target.files[0];
+              if (file) {
+                setImage(file);
+              } else {
+                setImage(null);
+              }
+            }}
+          />
 
-        <input
-          type='file'
-          className='form-control'
-          style={{ display: 'none' }}
-          ref={fileInputRef}
-          accept='image/*'
-          onChange={(event) => {
-            const file = event.target.files[0];
-            if (file) {
-              setImage(file);
-            } else {
-              setImage(null);
-            }
-          }}
-        />
-      </form>
+          <div className='row mt-4'>
+            <button
+              className='col preview'
+              onClick={(event) => {
+                event.preventDefault();
+              }}
+            >
+              Preview
+            </button>
+            <button
+              className='col terbitkan'
+              onClick={(event) => {
+                event.preventDefault();
+              }}
+            >
+              Terbitkan
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
