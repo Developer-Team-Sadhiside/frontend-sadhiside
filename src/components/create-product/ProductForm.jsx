bc34c0fcf7df29828546ddc70c2c375a1e9694ed
Bagus Suprapta <69630915+slickeel@users.noreply.github.com>
2022-06-21 07:42:33 +0800

    style: back arrow

--- src/components/create-product/ProductForm.jsx
+++ src/components/create-product/ProductForm.jsx
@@ -28,86 +28,95 @@ export default function ProductForm() {
   return (
     <div className='create-product-container container'>
       <div className='row justify-content-sm-center'>
-        <form className='create-product-form col-md-6'>
-          <label className='create-product-label'>Nama Produk</label>
-          <input
-            type='text'
-            className='form-control'
-            placeholder='Nama Produk'
+        <div className='col-sm-1'>
+          <img
+            src='/svg/fi_arrow-left.svg'
+            alt=''
+            className="back"
           />
-          <label className='create-product-label'>Harga Produk</label>
-          <input
-            className='form-control'
-            placeholder={`Rp 0.00`}
-            value={price}
-            onChange={(event) => {
-              restrictAlphabets(event.target.value);
-            }}
-          />
-          <label className='create-product-label'>Kategori</label>
-          <select class='form-control'>
-            <option>Hobi</option>
-            <option>Kendaraan</option>
-            <option>Baju</option>
-            <option>Elektronik</option>
-            <option>Kesehatan</option>
-          </select>
-          <label className='create-product-label'>Deskripsi</label>
-          <textarea
-            type='text'
-            className='form-control description'
-            placeholder='Contoh: Jalan Ikan Hiu 33'
-          ></textarea>
+        </div>
+        <div className='col-md-6'>
+          <form className='create-product-form'>
+            <label className='create-product-label'>Nama Produk</label>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Nama Produk'
+            />
+            <label className='create-product-label'>Harga Produk</label>
+            <input
+              className='form-control'
+              placeholder={`Rp 0.00`}
+              value={price}
+              onChange={(event) => {
+                restrictAlphabets(event.target.value);
+              }}
+            />
+            <label className='create-product-label'>Kategori</label>
+            <select class='form-control'>
+              <option>Hobi</option>
+              <option>Kendaraan</option>
+              <option>Baju</option>
+              <option>Elektronik</option>
+              <option>Kesehatan</option>
+            </select>
+            <label className='create-product-label'>Deskripsi</label>
+            <textarea
+              type='text'
+              className='form-control description'
+              placeholder='Contoh: Jalan Ikan Hiu 33'
+            ></textarea>
 
-          <label className='create-product-label'>Foto Produk</label>
-          <div
-            className='picture'
-            onClick={() => {
-              fileInputRef.current.click();
-            }}
-          >
-            {preview ? (
-              <img src={preview} alt='' className='image-uploaded' />
-            ) : (
-              <img src='/svg/fi_plus.svg' alt='' className='plus-svg' />
-            )}
-          </div>
-
-          <input
-            type='file'
-            className='form-control'
-            style={{ display: 'none' }}
-            ref={fileInputRef}
-            accept='image/*'
-            onChange={(event) => {
-              const file = event.target.files[0];
-              if (file) {
-                setImage(file);
-              } else {
-                setImage(null);
-              }
-            }}
-          />
-
-          <div className='row mt-4'>
-            <button
-              className='col preview'
-              onClick={(event) => {
-                event.preventDefault();
+            <label className='create-product-label'>Foto Produk</label>
+            <div
+              className='picture'
+              onClick={() => {
+                fileInputRef.current.click();
               }}
             >
-              Preview
-            </button>
-            <button
-              className='col terbitkan'
-              onClick={(event) => {
-                event.preventDefault();
+              {preview ? (
+                <img src={preview} alt='' className='image-uploaded' />
+              ) : (
+                <img src='/svg/fi_plus.svg' alt='' className='plus-svg' />
+              )}
+            </div>
+
+            <input
+              type='file'
+              className='form-control'
+              style={{ display: 'none' }}
+              ref={fileInputRef}
+              accept='image/*'
+              onChange={(event) => {
+                const file = event.target.files[0];
+                if (file) {
+                  setImage(file);
+                } else {
+                  setImage(null);
+                }
               }}
-            >
-              Terbitkan
-            </button>
-          </div>
-        </form>
+            />
+
+            <div className='row mt-4'>
+              <button
+                className='col preview'
+                onClick={(event) => {
+                  event.preventDefault();
+                }}
+              >
+                Preview
+              </button>
+              <button
+                className='col terbitkan'
+                onClick={(event) => {
+                  event.preventDefault();
+                }}
+              >
+                Terbitkan
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/CreateProductPage.jsx
+++ src/pages/CreateProductPage.jsx
@@ -1,10 +1,8 @@
 import ProductForm from '../components/create-product/ProductForm';
-import Navbar from '../components/navbar/navbar';
 
 export default function CreateProductPage() {
   return (
     <div>
-      <Navbar/>
       <ProductForm/>
     </div>
   );
