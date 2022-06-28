4e37381ef295ba0b164f1818e68d75e30405514a
narantyo maulana <olandeoxys@gmail.com>
2022-06-28 20:10:06 +0700

    [Update] CategorySeller.jsx

--- src/components/seller/CategorySeller.jsx
+++ src/components/seller/CategorySeller.jsx
@@ -1,60 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../../assets/styles/CategorySeller.css"
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
+
 
 const CategorySeller = () => {
+const navigate = useNavigate();
+const [buttonGroup, setButtonGroup] = useState("Semua")
+
 return (
-<div className='container'>
-  <div className='row justify-content-center' style={{ marginTop:"100px" }}>
-    <div className='col-lg-10'>
-      <div className='col-lg-3'>
-        <div className='card mt-4' style={{ borderRadius:"16px" }}>
-          <div className='card-body'>
-            <p className='fw-bold'>Kategori</p>
-            <div className='row'>
-              <div className='col-1'>
-                <i className="bi bi-box mt-1"></i>
-              </div>
-              <div className='col-8 mt-1'>
-                <p>Semua Produk</p>
-              </div>
-              <div className='col-1'>
-                <i className="bi bi-chevron-right"></i>
-              </div>
+<div className='container' style={{ marginTop:'90px' }}>
+  <div className='row'>
+    <div className='col-lg-3 p-0 py-2' style={{ marginLeft:"85px" }}>
+      <div className='category-product-list'>
+        <ToggleButtonGroup type="radio" name="category-mobile-version" value={buttonGroup}
+          className='button-group-custom' onChange={(event)=> {
+          setButtonGroup(event)}}>
+          <h6 className='mb-3 text-category'>Kategori</h6>
+          <ToggleButton id="radio-button-1" value={"Semua Produk"}  className='btn-group-category bb-category'>
+            <div className='d-flex justify-content-center align-items-center'>
+              <img src="/images/Box_icon.png" className='me-2' style={{width: '25px'}} alt="" />
+              <p className='m-0' >Semua Produk</p>
             </div>
-            <div className='row'>
-              <div className='col-1'>
-                <i className="bi bi-heart"></i>
-              </div>
-              <div className='col-8'>
-                <p>Diminati</p>
-              </div>
-              <div className='col-1'>
-                <i className="bi bi-chevron-right"></i>
-              </div>
+            <img src="/images/Chevron-Right.png" className='right-arrow' style={{width: '25px'}} alt="" />
+          </ToggleButton>
+          <ToggleButton id="radio-button-2" value={"Diminati"} className='btn-group-category bb-category'>
+            <div className='d-flex justify-content-center align-items-center'>
+              <img src="/images/Heart_icon.png" className='me-2' style={{width: '25px'}} alt="" />
+              <p className='m-0'>Diminati</p>
             </div>
-            <div className='row'>
-              <div className='col-1'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-currency-dollar" viewBox="0 0 16 16">
-                  <path
-                    d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z" />
-                </svg>
-              </div>
-              <div className='col-8'>
-                <p>Terjual</p>
-              </div>
-              <div className='col-1'>
-                <i className="bi bi-chevron-right"></i>
-              </div>
+            <img src="/images/Chevron-Right.png" className='right-arrow' style={{width: '25px'}} alt="" />
+          </ToggleButton>
+          <ToggleButton id="radio-button-3" value={"Terjual"} className='btn-group-category'>
+            <div className='d-flex justify-content-center align-items-center'>
+              <img src="/images/Dollar_icon.png" className='me-2' style={{width: '25px'}} alt="" />
+              <p className='m-0'>Terjual</p>
             </div>
+            <img src="/images/Chevron-Right.png" className='right-arrow' style={{width: '25px'}} alt="" />
+          </ToggleButton>
+        </ToggleButtonGroup>
+        {console.log("Mobile Button Value: ", buttonGroup)}
+      </div>
+    </div>
+    <div className="col-lg-9 p-0">
+      <div className="row">
+        <div className="col">
+          <div className='add-product-box p-2 text-center text-secondary'onClick={()=>
+            {navigate("/products/create")}}>
+            <img src="/images/Plus_icon.png" style={{ width:"30px" }} className= 'mb-3' alt="" />
+            <p>Tambah Produk</p>
           </div>
-        </div>
-        <div className="add_product">
-          <Link to="/products/create">
-          <img src='/svg/fi_plus.svg' alt='' className='add-svg' />
-          </Link>
-          <p className='text_add_product'>Tambah Produk</p>
         </div>
       </div>
     </div>
