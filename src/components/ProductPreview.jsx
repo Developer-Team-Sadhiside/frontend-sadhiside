f3f7ea72d09bb247c41499b49d41ee169f36438e
Wahyu Priyo Atmaja <wahyupriyoatmaja7@gmail.com>
2022-07-04 09:07:08 +0700

    update offermodal

--- src/components/ProductPreview.jsx
+++ src/components/ProductPreview.jsx
@@ -1,7 +1,7 @@
 import "../assets/styles/ProductPreview.css";
 import { users } from "../assets/dumps/users";
 import { products } from "../assets/dumps/products";
-import Offer_Modal from "./OfferModal/OfferModal";
+import Offer_Modal from "./buyer/OfferModal";
 import "../assets/styles/OfferModal.css";
 
 import { Link, useParams } from "react-router-dom";
@@ -10,46 +10,21 @@ export function ProductCarouselPreview() {
   const params = useParams();
 
   return (
-    <div
-      id="productCarouselPreviewControl"
-      className="carousel slide product-carousel-preview"
-    >
+    <div id="productCarouselPreviewControl" className="carousel slide product-carousel-preview">
       <div className="carousel-inner product-carousel-image-preview">
         <div className="carousel-item active">
-          <img
-            src={products[params.id]?.image ?? products[0].image}
-            className="product-carousel-image-size-preview"
-          />
+          <img src={products[params.id]?.image ?? products[0].image} className="product-carousel-image-size-preview" />
         </div>
         <div className="carousel-item">
-          <img
-            src={products[params.id]?.image ?? products[0].image}
-            className="product-carousel-image-size-preview"
-          />
+          <img src={products[params.id]?.image ?? products[0].image} className="product-carousel-image-size-preview" />
         </div>
       </div>
       <div className="product-carousel-button-preview">
-        <button
-          className="carousel-control-prev"
-          data-bs-target="#productCarouselPreviewControl"
-          data-bs-slide="prev"
-        >
-          <img
-            src="/svg/fi_product-button-prev-preview.svg"
-            alt="Previous Button"
-            className="product-carousel-button-size-preview"
-          />
+        <button className="carousel-control-prev" data-bs-target="#productCarouselPreviewControl" data-bs-slide="prev">
+          <img src="/svg/fi_product-button-prev-preview.svg" alt="Previous Button" className="product-carousel-button-size-preview" />
         </button>
-        <button
-          className="carousel-control-next"
-          data-bs-target="#productCarouselPreviewControl"
-          data-bs-slide="next"
-        >
-          <img
-            src="/svg/fi_product-button-next-preview.svg"
-            alt="Next Button"
-            className="product-carousel-button-size-preview"
-          />
+        <button className="carousel-control-next" data-bs-target="#productCarouselPreviewControl" data-bs-slide="next">
+          <img src="/svg/fi_product-button-next-preview.svg" alt="Next Button" className="product-carousel-button-size-preview" />
         </button>
       </div>
     </div>
@@ -62,9 +37,7 @@ export function ProductDescriptionPreview() {
   return (
     <div className="product-description-preview">
       <h3 className="product-description-heading-preview">Deskripsi</h3>
-      <p className="product-description-description-preview">
-        {products[params.id]?.description ?? products[0].description}
-      </p>
+      <p className="product-description-description-preview">{products[params.id]?.description ?? products[0].description}</p>
     </div>
   );
 }
@@ -82,12 +55,7 @@ export function EditButtonPreview() {
 export function PurpleButton(props) {
   return (
     <Link to="">
-      <button
-        type="submit"
-        className="product-card-button-publish-preview"
-        data-bs-toggle="modal"
-        data-bs-target="#modal"
-      >
+      <button type="submit" className="product-card-button-publish-preview" data-bs-toggle="modal" data-bs-target="#modal">
         {props.content}
       </button>
     </Link>
@@ -99,17 +67,9 @@ export function ProductCardPreview() {
 
   return (
     <div className="product-card-preview">
-      <p className="product-card-heading-preview">
-        {products[params.id]?.title ?? products[0].title}
-      </p>
-      <p className="product-card-category-preview">
-        {products[params.id]?.category ?? products[0].category}
-      </p>
-      <p className="product-card-price-preview">
-        Rp.{" "}
-        {products[params.id]?.price.toLocaleString("id-ID") ??
-          products[0].price}
-      </p>
+      <p className="product-card-heading-preview">{products[params.id]?.title ?? products[0].title}</p>
+      <p className="product-card-category-preview">{products[params.id]?.category ?? products[0].category}</p>
+      <p className="product-card-price-preview">Rp. {products[params.id]?.price.toLocaleString("id-ID") ?? products[0].price}</p>
       <div className="product-card-button-preview">
         {params.id ? (
           <PurpleButton content="Saya Tertarik dan Ingin Nego" />
@@ -130,19 +90,11 @@ export function UserCardPreview() {
   return (
     <div className="user-card-preview row g-0">
       <div className="user-card-image-preview col-sm-3">
-        <img
-          src={users[params.id]?.profilePicture ?? users[0]?.profilePicture}
-          alt="User Image"
-          className="col-sm-3 user-card-image-size-preview"
-        />
+        <img src={users[params.id]?.profilePicture ?? users[0]?.profilePicture} alt="User Image" className="col-sm-3 user-card-image-size-preview" />
       </div>
       <div className="col user-card-identity-preview">
-        <div className="user-card-identity-name-preview">
-          {users[params.id]?.name ?? users[0]?.name}
-        </div>
-        <div className="user-card-identity-city-preview">
-          {users[params.id]?.city ?? users[0]?.city}
-        </div>
+        <div className="user-card-identity-name-preview">{users[params.id]?.name ?? users[0]?.name}</div>
+        <div className="user-card-identity-city-preview">{users[params.id]?.city ?? users[0]?.city}</div>
       </div>
     </div>
   );
