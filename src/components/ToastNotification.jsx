747b3d9dcfe764b958b9ebaeb956016c6a555e32
Bagus Suprapta <69630915+slickeel@users.noreply.github.com>
2022-07-07 23:26:25 +0800

    fix: toast notification icon

--- src/components/ToastNotification.jsx
+++ src/components/ToastNotification.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import "../assets/styles/ToastNotification.css";
+import '../assets/styles/ToastNotification.css';
 
-function ToastNotification() {
+function Notification(props) {
   return (
-    <div className="fixed-top container">
-      <div className="alert alert-success alert-dismissible fade show text-white col-md-6 offset-md-3 align-self-center" role="alert">
-        Produk Berhasil ditambahkan
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
+    <div className='alert alert-success alert-dismissible fade show text-white col-md-6 offset-md-3 align-self-center' role='alert'>
+      <p className='notification-message'>{props.content}</p>
+      <img src='/svg/fi_x.svg' alt='' data-bs-dismiss='alert' aria-label='Close' className='notification-button-close' />
     </div>
   );
 }
 
-export default ToastNotification;
+export default function ToastNotification(props) {
+  return (
+    <div className='fixed-top container toast-notification-container'>
+      <Notification content={props.content} />
+    </div>
+  );
+}
