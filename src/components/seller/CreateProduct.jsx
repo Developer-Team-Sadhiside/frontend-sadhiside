a7cf7c7176cc64872a5389330d895cd44daba168
narantyo maulana <olandeoxys@gmail.com>
2022-07-11 19:30:29 +0700

    [ADD] integration Api create product

--- src/components/seller/CreateProduct.jsx
+++ src/components/seller/CreateProduct.jsx
@@ -1,29 +1,62 @@
 import '../../assets/styles/CreateProduct.css';
 import { useRef, useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function CreateProduct() {
   const [price, setPrice] = useState();
-
+  const [productName, setProductName] = useState("");
+  const [category, setCategory] = useState("");
+  const [desc, setDesc] = useState("");
   const [image, setImage] = useState();
-  const [preview, setPreview] = useState();
+  // const [preview, setPreview] = useState();
+  const [preview2, setPreview2] = useState();
+  const navigate = useNavigate();
   const fileInputRef = useRef();
+
+
+  const loadImage = (e) => {
+    const image = e.target.files[0];
+    setImage(image);
+    setPreview2(URL.createObjectURL(image));
+  }
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("productName", productName);
+    formData.append("price", price);
+    formData.append("category", category);
+    formData.append("desc", desc);
+    try {
+      await axios.post("http://localhost8000/api/v1/addProduct", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
+      navigate("/products/create/preview");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     if (image) {
       const reader = new FileReader();
       reader.readAsDataURL(image);
       reader.onload = function (e) {
-        setPreview(reader.result);
+        setPreview2(reader.result);
       };
     } else {
-      setPreview(null);
+      setPreview2(null);
     }
   }, [image]);
 
-  function restrictAlphabets(input) {
-    const value = input.replace(/\D/g, '');
-    setPrice(value);
-  }
+  // function restrictAlphabets(input) {
+  //   const value = input.replace(/\D/g, '');
+  //   setPrice(value);
+  // }
 
   return (
     <div className='create-product-container container mt-3'>
@@ -36,86 +69,76 @@ export default function CreateProduct() {
           />
         </div>
         <div className='col-sm-6'>
-          <form className='create-product-form'>
-            <label className='create-product-label'>Nama Produk</label>
-            <input
-              type='text'
-              className='form-control'
-              placeholder='Nama Produk'
-            />
-            <label className='create-product-label'>Harga Produk</label>
-            <input
-              className='form-control'
-              placeholder={`Rp 0.00`}
-              value={price}
-              onChange={(event) => {
-                restrictAlphabets(event.target.value);
-              }}
-            />
-            <label className='create-product-label'>Kategori</label>
-            <select class='form-control'>
-              <option>Hobi</option>
-              <option>Kendaraan</option>
-              <option>Baju</option>
-              <option>Elektronik</option>
-              <option>Kesehatan</option>
-            </select>
-            <label className='create-product-label'>Deskripsi</label>
-            <textarea
-              type='text'
-              className='form-control description'
-              placeholder='Contoh: Jalan Ikan Hiu 33'
-            ></textarea>
+          <div className='create-product-form'>
+            <form onSubmit={saveProduct}>
+              <label className='create-product-label'>Nama Produk</label>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Nama Produk'
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+              />
+              <label className='create-product-label'>Harga Produk</label>
+              <input
+                className='form-control'
+                placeholder={`Rp 0.00`}
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <label className='create-product-label'>Kategori</label>
+              <select class='form-control' value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option>Hobi</option>
+                <option>Kendaraan</option>
+                <option>Baju</option>
+                <option>Elektronik</option>
+                <option>Kesehatan</option>
+              </select>
+              <label className='create-product-label'>Deskripsi</label>
+              <textarea
+                type='text'
+                className='form-control description'
+                placeholder='Contoh: Jalan Ikan Hiu 33'
+                value={desc}
+                onChange= {(e) => setDesc(e.target.value)}
+              ></textarea>
 
-            <label className='create-product-label'>Foto Produk</label>
-            <div
-              className='picture'
-              onClick={() => {
-                fileInputRef.current.click();
-              }}
-            >
-              {preview ? (
-                <img src={preview} alt='' className='image-uploaded' />
-              ) : (
-                <img src='/svg/fi_plus.svg' alt='' className='plus-svg' />
-              )}
-            </div>
-
-            <input
-              type='file'
-              className='form-control'
-              style={{ display: 'none'}}
-              ref={fileInputRef}
-              accept='image/*'
-              onChange={(event) => {
-                const file = event.target.files[0];
-                if (file) {
-                  setImage(file);
-                } else {
-                  setImage(null);
-                }
-              }}
-            />
-
-            <div className='row mt-4'>
-              <button
-                className='col preview'
-                onClick={(event) => {
-                  event.preventDefault();
+              <label className='create-product-label'>Foto Produk</label>
+              <div
+                className='picture'
+                onChange={loadImage}
+                onClick={() => {
+                  fileInputRef.current.click();
                 }}
               >
-                Preview
-              </button>
-              <button
-                className='col terbitkan'
-                onClick={(event) => {
-                  event.preventDefault();
+                {preview2 ? (
+                  <img src={preview2} alt='' className='image-uploaded' />
+                ) : (
+                  <img src='/svg/fi_plus.svg' alt='' className='plus-svg' />
+                )}
+              </div>
+
+              <input
+                type='file'
+                className='form-control'
+                style={{ display: 'none'}}
+                ref={fileInputRef}
+                accept='image/*'
+                onChange={(event) => {
+                  const file = event.target.files[0];
+                  if (file) {
+                    setImage(file);
+                  } else {
+                    setImage(null);
+                  }
                 }}
-              >
-                Terbitkan
-              </button>
-            </div>
-          </form>
+              />
+                <div className='row mt-4'>
+                  <button className='col preview'>Preview</button>
+                  <button className='col terbitkan' type='submit'>Terbitkan</button>
+                </div>
+            </form>
+          </div>
         </div>
         <div className='col-sm-1'></div>
       </div>
