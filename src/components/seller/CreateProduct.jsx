95f82ae72c00cea5ab57e3f9a5376dced0475224
narantyo maulana <olandeoxys@gmail.com>
2022-07-12 19:38:45 +0700

    [Update] Create Product

--- src/components/seller/CreateProduct.jsx
+++ src/components/seller/CreateProduct.jsx
@@ -4,33 +4,33 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function CreateProduct() {
-  const [price, setPrice] = useState();
-  const [productName, setProductName] = useState("");
-  const [category, setCategory] = useState("");
-  const [desc, setDesc] = useState("");
-  const [image, setImage] = useState();
+  const [deskripsi, setDeskripsi] = useState();
+  const [nama_produk, setNama_Produk] = useState("");
+  const [harga_produk, setHarga_Produk] = useState("");
+  const [kategori, setKategori] = useState("");
+  const [file, setFile] = useState("");
   // const [preview, setPreview] = useState();
-  const [preview2, setPreview2] = useState();
+  const [preview, setPreview] = useState();
   const navigate = useNavigate();
   const fileInputRef = useRef();
 
 
   const loadImage = (e) => {
     const image = e.target.files[0];
-    setImage(image);
-    setPreview2(URL.createObjectURL(image));
-  }
+    setFile(image);
+    setPreview(URL.createObjectURL(image));
+  };
 
-  const saveProduct = async (e) => {
+  const createProducts = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("image", image);
-    formData.append("productName", productName);
-    formData.append("price", price);
-    formData.append("category", category);
-    formData.append("desc", desc);
+    formData.append("file", file);
+    formData.append("nama_produk", nama_produk);
+    formData.append("harga_produk", harga_produk);
+    formData.append("kategori", kategori);
+    formData.append("deskripsi", deskripsi);
     try {
-      await axios.post("http://localhost8000/api/v1/addProduct", formData, {
+      await axios.post("http://localhost:8000/api/v1/addProduct", formData, {
         headers: {
           "Content-type": "multipart/form-data",
         },
@@ -42,16 +42,16 @@ export default function CreateProduct() {
   };
 
   useEffect(() => {
-    if (image) {
+    if (file) {
       const reader = new FileReader();
-      reader.readAsDataURL(image);
+      reader.readAsDataURL(file);
       reader.onload = function (e) {
-        setPreview2(reader.result);
+        setPreview(reader.result);
       };
     } else {
-      setPreview2(null);
+      setPreview(null);
     }
-  }, [image]);
+  }, [file]);
 
   // function restrictAlphabets(input) {
   //   const value = input.replace(/\D/g, '');
@@ -70,24 +70,24 @@ export default function CreateProduct() {
         </div>
         <div className='col-sm-6'>
           <div className='create-product-form'>
-            <form onSubmit={saveProduct}>
+            <form onSubmit={createProducts}>
               <label className='create-product-label'>Nama Produk</label>
               <input
                 type='text'
                 className='form-control'
                 placeholder='Nama Produk'
-                value={productName}
-                onChange={(e) => setProductName(e.target.value)}
+                value={nama_produk}
+                onChange={(e) => setNama_Produk(e.target.value)}
               />
               <label className='create-product-label'>Harga Produk</label>
               <input
                 className='form-control'
                 placeholder={`Rp 0.00`}
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
+                value={harga_produk}
+                onChange={(e) => setHarga_Produk(e.target.value)}
               />
               <label className='create-product-label'>Kategori</label>
-              <select class='form-control' value={category} onChange={(e) => setCategory(e.target.value)}>
+              <select class='form-control' value={kategori} onChange={(e) => setKategori(e.target.value)}>
                 <option>Hobi</option>
                 <option>Kendaraan</option>
                 <option>Baju</option>
@@ -99,8 +99,8 @@ export default function CreateProduct() {
                 type='text'
                 className='form-control description'
                 placeholder='Contoh: Jalan Ikan Hiu 33'
-                value={desc}
-                onChange= {(e) => setDesc(e.target.value)}
+                value={deskripsi}
+                onChange= {(e) => setDeskripsi(e.target.value)}
               ></textarea>
 
               <label className='create-product-label'>Foto Produk</label>
@@ -111,8 +111,8 @@ export default function CreateProduct() {
                   fileInputRef.current.click();
                 }}
               >
-                {preview2 ? (
-                  <img src={preview2} alt='' className='image-uploaded' />
+                {preview ? (
+                  <img src={preview} alt='' className='image-uploaded' />
                 ) : (
                   <img src='/svg/fi_plus.svg' alt='' className='plus-svg' />
                 )}
@@ -127,9 +127,9 @@ export default function CreateProduct() {
                 onChange={(event) => {
                   const file = event.target.files[0];
                   if (file) {
-                    setImage(file);
+                    setFile(file);
                   } else {
-                    setImage(null);
+                    setFile(null);
                   }
                 }}
               />
