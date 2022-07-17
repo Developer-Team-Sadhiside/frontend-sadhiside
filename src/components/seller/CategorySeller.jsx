8d444b00991b7256b51f797c8ff053211d120776
Wahyu Priyo Atmaja <wahyupriyoatmaja7@gmail.com>
2022-07-17 20:11:46 +0700

    integrasi api registerSeller

--- src/components/seller/CategorySeller.jsx
+++ src/components/seller/CategorySeller.jsx
@@ -1,106 +1,122 @@
-import React, {useState} from 'react'
-import "../../assets/styles/CategorySeller.css"
-import { useNavigate } from 'react-router-dom'
-import { ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
-
+import React, { useState } from "react";
+import "../../assets/styles/CategorySeller.css";
+import { useNavigate } from "react-router-dom";
+import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 const CategorySeller = () => {
+  const [all, setAll] = useState(true);
+  const [like, setLike] = useState(false);
+  const [sell, setSell] = useState(false);
 
-const [all, setAll] = useState(true)
-const [like, setLike] = useState(false)
-const [sell, setSell] = useState(false)
+  const colorActive = {
+    color: "#7126B5",
+  };
+  const colorInactive = {
+    color: "black",
+  };
 
-const colorActive = {
-  color: '#7126B5',
-};
-const colorInactive = {
-  color: 'black',
-};
+  const handleAll = (e) => {
+    e.preventDefault();
+    setAll(true);
+    setLike(false);
+    setSell(false);
+  };
 
+  const handleLike = (e) => {
+    e.preventDefault();
+    setAll(false);
+    setLike(true);
+    setSell(false);
+  };
 
-const handleAll = (e) =>{
-  e.preventDefault()
-  setAll(true)
-  setLike(false)
-  setSell(false)
-}
+  const handleSell = (e) => {
+    e.preventDefault();
+    setAll(false);
+    setLike(false);
+    setSell(true);
+  };
 
-const handleLike = (e) => {
-  e.preventDefault()
-  setAll(false)
-  setLike(true)
-  setSell(false)
-}
+  const navigate = useNavigate();
+  const [buttonGroup, setButtonGroup] = useState("Semua");
 
-const handleSell = (e) => {
-  e.preventDefault()
-  setAll(false)
-  setLike(false)
-  setSell(true)
-}
-
-const navigate = useNavigate();
-const [buttonGroup, setButtonGroup] = useState("Semua")
-
-return (
-<div className='container' style={{ marginTop:'90px' }}>
-  <div className='row'>
-    <div className='col-lg-3 p-0 py-2' style={{ marginLeft:"85px" }}>
-      <div className='category-product-list'>
-        <ToggleButtonGroup type="radio" name="category-mobile-version" value={buttonGroup}
-          className='button-group-custom' onChange={(event)=> {
-          setButtonGroup(event)}}>
-          <h6 className='mb-3 text-category'>Kategori</h6>
-          <div id="radio-button-1" value={"Semua Produk"}  onClick={handleAll}  className='btn-group-category bb-category'>
-            <div className='d-flex justify-content-center align-items-center' style={ all ? colorActive : colorInactive }>
-            <i class="me-2 fa-solid fa-box"></i>
-              <p className='m-0' >Semua Produk</p>
-            </div>
-            <i className="fa-solid fa-angle-right"style={ all ? colorActive : colorInactive }></i>
-          </div>
-
-          <div id="radio-button-2" value={"Diminati"} onClick={handleLike} className='btn-group-category bb-category'>
-            <div className='d-flex justify-content-center align-items-center' style={ like ? colorActive : colorInactive }>
-              <i className="me-2 fa-solid fa-heart"></i>
-              <p className='m-0'>Diminati</p>
-            </div>
-            <i className="fa-solid fa-angle-right"style={ like ? colorActive : colorInactive }></i>
-          </div>
-          <div id="radio-button-3" value={"Terjual"} onClick={handleSell} className='btn-group-category'>
-            <div className='d-flex justify-content-center align-items-center' style={ sell ? colorActive : colorInactive }>
-              <i className="me-2 fa-solid fa-dollar-sign"></i>
-              <p className='m-0'>Terjual</p>
-            </div>
-            <i className="fa-solid fa-angle-right"style={ sell ? colorActive : colorInactive }></i>
-          </div>
-        </ToggleButtonGroup>
-        {console.log("Mobile Button Value: ", buttonGroup)}
-      </div>
-    </div>
-    <div className="col-lg-9 p-0">
+  return (
+    <div className="container" style={{ marginTop: "90px" }}>
       <div className="row">
-        {all && (<div className="col">
-          <div className='add-product-box p-2 text-center text-secondary'onClick={()=>
-            {navigate("/products/create")}}>
-            <img src="/images/Plus_icon.png" style={{ width:"30px" }} className= 'mb-3' alt="" />
-            <p>Semua Produk</p>
+        <div className="col-lg-3 p-0 py-2" style={{ marginLeft: "85px" }}>
+          <div className="category-product-list">
+            <ToggleButtonGroup
+              type="radio"
+              name="category-mobile-version"
+              value={buttonGroup}
+              className="button-group-custom"
+              onChange={(event) => {
+                setButtonGroup(event);
+              }}
+            >
+              <h6 className="mb-3 text-category">Kategori</h6>
+              <div id="radio-button-1" value={"Semua Produk"} onClick={handleAll} className="btn-group-category bb-category">
+                <div className="d-flex justify-content-center align-items-center" style={all ? colorActive : colorInactive}>
+                  <i class="me-2 fa-solid fa-box"></i>
+                  <p className="m-0">Semua Produk</p>
+                </div>
+                <i className="fa-solid fa-angle-right" style={all ? colorActive : colorInactive}></i>
+              </div>
+
+              <div id="radio-button-2" value={"Diminati"} onClick={handleLike} className="btn-group-category bb-category">
+                <div className="d-flex justify-content-center align-items-center" style={like ? colorActive : colorInactive}>
+                  <i className="me-2 fa-solid fa-heart"></i>
+                  <p className="m-0">Diminati</p>
+                </div>
+                <i className="fa-solid fa-angle-right" style={like ? colorActive : colorInactive}></i>
+              </div>
+              <div id="radio-button-3" value={"Terjual"} onClick={handleSell} className="btn-group-category">
+                <div className="d-flex justify-content-center align-items-center" style={sell ? colorActive : colorInactive}>
+                  <i className="me-2 fa-solid fa-dollar-sign"></i>
+                  <p className="m-0">Terjual</p>
+                </div>
+                <i className="fa-solid fa-angle-right" style={sell ? colorActive : colorInactive}></i>
+              </div>
+            </ToggleButtonGroup>
+            {console.log("Mobile Button Value: ", buttonGroup)}
           </div>
-        </div>)}
-        {like && (<div className="col">
-          <div className='most-like'>
-            <img src="/svg/empty-product-illustration.svg" className= 'mt-2' alt="" />   
+        </div>
+        <div className="col-lg-9 p-0">
+          <div className="row">
+            {all && (
+              <div className="col">
+                <div
+                  className="add-product-box p-2 text-center text-secondary"
+                  onClick={() => {
+                    navigate("/products/create");
+                  }}
+                >
+                  <img src="/images/Plus_icon.png" style={{ width: "30px" }} className="mb-3" alt="" />
+                  <p>Semua Produk</p>
+                </div>
+              </div>
+            )}
+            {like && (
+              <div className="col">
+                <div className="most-like">
+                  <img src="/svg/empty-product-illustration.svg" className="mt-2" alt="" />
+                </div>
+              </div>
+            )}
+            {sell && (
+              <div className="col">
+                <div
+                  className="sold"
+                  onClick={() => {
+                    navigate("/dashboard/seller/sell");
+                  }}
+                ></div>
+              </div>
+            )}
           </div>
-        </div>)}
-        {sell && (<div className="col">
-          <div className='sold'onClick={()=>
-            {navigate("/dashboard/seller/sell")}}>
-          </div>
-        </div>)}
+        </div>
       </div>
     </div>
-  </div>
-</div>
-)
-}
+  );
+};
 
-export default CategorySeller
+export default CategorySeller;
--- src/components/seller/RegisterAsSeller.jsx
+++ src/components/seller/RegisterAsSeller.jsx
@@ -1,108 +1,143 @@
-import React, { useState, useEffect, useRef } from 'react';
-import '../../assets/styles/RegisterAsSeller.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../../assets/styles/RegisterAsSeller.css";
 
 const Profile = () => {
-  const [image, setImage] = useState();
+  const [foto, setFoto] = useState();
   const [preview, setPreview] = useState();
+  const [nama, setNama] = useState();
+  const [kota, setKota] = useState();
+  const [alamat, setAlamat] = useState();
+  const [no_hp, setNohp] = useState();
   const fileInputRef = useRef();
+  const navigate = useNavigate();
+
+  const saveProfil = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("foto", foto);
+    formData.append("nama", nama);
+    formData.append("kota", kota);
+    formData.append("alamat", alamat);
+    formData.append("no_hp", no_hp);
+
+    const token = localStorage.getItem("token");
+    console.log(token);
+
+    try {
+      await axios.put("http://localhost:8000/api/v1/users/addProfil", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      navigate("/dashboard/seller");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    if (image) {
+    if (foto) {
       const reader = new FileReader();
-      reader.readAsDataURL(image);
+      reader.readAsDataURL(foto);
       reader.onload = function (e) {
         setPreview(reader.result);
       };
     } else {
       setPreview(null);
     }
-  }, [image]);
+  }, [foto]);
 
   return (
-    <div className='profile-container'>
-      <div className='col-sm'>
-        <img
-          src='/svg/fi_arrow-left.svg'
-          alt=''
-          className='back'
-          style={{ marginTop: '30px', marginLeft: '-50px' }}
-        />
+    <div className="profile-container">
+      <div className="col-sm">
+        <img src="/svg/fi_arrow-left.svg" alt="" className="back" style={{ marginTop: "30px", marginLeft: "-50px" }} />
       </div>
       <div
-        className='picture'
+        className="picture"
         onClick={() => {
           fileInputRef.current.click();
         }}
       >
-        {preview ? (
-          <img src={preview} alt='' className='image-uploaded' />
-        ) : (
-          <img src='/svg/fi-blank-profile.svg' alt='' className='plus-svg' />
-        )}
+        {preview ? <img src={preview} alt="" className="image-uploaded" /> : <img src="/svg/fi-blank-profile.svg" alt="" className="plus-svg" />}
       </div>
       <input
-        type='file'
-        className='form-control'
-        style={{ display: 'none' }}
+        type="file"
+        className="form-control"
+        style={{ display: "none" }}
         ref={fileInputRef}
-        accept='image/*'
+        accept="image/*"
         onChange={(event) => {
           const file = event.target.files[0];
           if (file) {
-            setImage(file);
+            setFoto(file);
           } else {
-            setImage(null);
+            setFoto(null);
           }
         }}
       />
       <form>
-        <div className='profile-form mt-4'>
-          <label className='form-label' htmlFor='nama'>
+        <div className="profile-form mt-4">
+          <label className="form-label" htmlFor="nama">
             Nama*
           </label>
           <input
-            type='text'
-            className='form-control'
-            placeholder='Nama'
-            id='nama-lengkap'
+            type="text"
+            className="form-control ml-4"
+            onChange={(event) => {
+              setNama(event.target.value);
+            }}
+            placeholder="Nama"
+            id="nama-lengkap"
           />
-          <label className='form-label mt-4' htmlFor='kota'>
+          <label className="form-label mt-4" htmlFor="no-hp">
             Kota*
           </label>
-          <select class='form-select' id='inlineFormSelectPref'>
-            <option selected>Pilih Kota</option>
-            <option value='1'>Surabaya</option>
-            <option value='2'>Yogyakarta</option>
-            <option value='3'>Jakarta</option>
-          </select>
-          <label className='form-label mt-4' htmlFor='alamat'>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(event) => {
+              setKota(event.target.value);
+            }}
+            placeholder="contoh: demak"
+            id="kota"
+          />
+          <label
+            className="form-label mt-4"
+            onChange={(event) => {
+              setAlamat(event.target.value);
+            }}
+            htmlFor="alamat"
+          >
             Alamat*
           </label>
           <input
-            type='text'
-            className='form-control'
-            style={{ height: '80px', fontSize: '14px' }}
-            placeholder='Contoh: Jalan Ikan Hiu 33'
-            id='alamat'
+            type="text"
+            className="form-control"
+            onChange={(event) => {
+              setAlamat(event.target.value);
+            }}
+            style={{ height: "80px", fontSize: "14px" }}
+            placeholder="Contoh: Jalan Ikan Hiu 33"
+            id="alamat"
           />
-          <label className='form-label mt-4' htmlFor='no-hp'>
+          <label className="form-label mt-4" htmlFor="no-hp">
             No Handphone*
           </label>
           <input
-            type='text'
-            className='form-control'
-            placeholder='contoh: +628123456789'
-            id='no-hp'
+            type="text"
+            className="form-control"
+            onChange={(event) => {
+              setNohp(event.target.value);
+            }}
+            placeholder="contoh: +628123456789"
+            id="no_hp"
           />
         </div>
-        <div className='justify-content-center mb-5'>
-          <button
-            type='button'
-            className='btn btn-simpan'
-            style={{ color: '#FFFFFF' }}
-          >
-            Simpan
-          </button>
+        <div className="justify-content-center mb-5">
+          <input onClick={saveProfil} type="submit" className="btn btn-simpan" value="SImpan" />
         </div>
       </form>
     </div>
