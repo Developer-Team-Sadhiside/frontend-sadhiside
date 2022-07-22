23fd7d6ea22d46f952be40f11b934849697e9810
Wahyu Priyo Atmaja <wahyupriyoatmaja7@gmail.com>
2022-07-22 21:23:13 +0700

    fix sell button

--- src/components/SellButton.jsx
+++ src/components/SellButton.jsx
@@ -1,18 +1,18 @@
-import '../assets/styles/SellButton.css';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import "../assets/styles/SellButton.css";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 export default function SellButton() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const navigate = useNavigate();
-  const [kota, setKota] = useState('');
-  const [alamat, setAlamat] = useState('');
+  const [kota, setKota] = useState("");
+  const [alamat, setAlamat] = useState("");
   useState(async () => {
-    const response = await axios.get('http://localhost:8000/api/v1/users/whoAmI', {
+    const response = await axios.get("http://localhost:8000/api/v1/users/whoAmI", {
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: "Bearer " + token,
       },
     });
     setKota(response.data.user.kota);
@@ -20,25 +20,27 @@ export default function SellButton() {
   });
 
   function isLogin() {
-    if (!localStorage.getItem(token)) return false;
+    if (!localStorage.getItem(token)) {
+      navigate("/login");
+    }
   }
 
   console.log(kota, alamat);
   return (
-    <a href='' className='start-sell-link'>
+    <a href="" className="start-sell-link">
       <button
-        className='fixed-bottom container start-sell-link-button'
+        className="fixed-bottom container start-sell-link-button"
         onClick={() => {
-          if (isLogin()) {
-            navigate('/login');
+          if (token === null) {
+            navigate("/login");
           } else if (kota === null || alamat === null) {
-            navigate('/addprofil');
+            navigate("/addprofil");
           } else {
-            navigate('/dashboard/seller/createproduct');
+            navigate("/dashboard/seller/createproduct");
           }
         }}
       >
-        <img src='/svg/fi_plus-white.svg' alt='Plus Icon' /> Jual
+        <img src="/svg/fi_plus-white.svg" alt="Plus Icon" /> Jual
       </button>
     </a>
   );
