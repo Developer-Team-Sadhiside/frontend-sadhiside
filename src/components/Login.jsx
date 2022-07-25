055be4095391710ec1d2b0c2e13b92acc45006f3
Bagus Suprapta <69630915+slickeel@users.noreply.github.com>
2022-07-25 13:21:38 +0800

    fix: loading login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,29 @@
-import React, { Fragment, useState } from "react";
-import { toast } from "react-toastify";
-import { Link, useNavigate } from "react-router-dom";
-import "../assets/styles/Login.css";
-import axios from "axios";
+import React, { Fragment, useState } from 'react';
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import '../assets/styles/Login.css';
+import axios from 'axios';
 
 const Login = () => {
   const [inputtext, setinputtext] = useState({
-    password: "",
+    password: '',
   });
 
   // Eye Flash Icon
   const [eye, seteye] = useState(true);
-  const [password, setPassword] = useState("password");
+  const [password, setPassword] = useState('password');
   const [type, settype] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   // Email and password
-  const [email, setEmail] = useState("");
-  const [password2, setPassword2] = useState("");
+  const [email, setEmail] = useState('');
+  const [password2, setPassword2] = useState('');
 
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   const navigate = useNavigate();
 
-  const domain = 'https://secondhand-shadiside.herokuapp.com'
+  const domain = 'https://secondhand-shadiside.herokuapp.com';
 
   const submitLogin = async (event) => {
     event.preventDefault();
@@ -33,12 +34,13 @@ const Login = () => {
 
     try {
       const response = await axios.post(`${domain}/api/v1/users/login`, data);
-      localStorage.setItem("token", response.data.token);
-      navigate("/");
+      localStorage.setItem('token', response.data.token);
+      navigate('/');
     } catch (error) {
       // toast.error('Bad User Credentials',{
       //   position: "top-center",
       // })
+      setLoading(false)
       setError(error.response.data.message);
     }
   };
@@ -46,13 +48,13 @@ const Login = () => {
   const onChangeEmail = (e) => {
     const value = e.target.value;
     setEmail(value);
-    setError("");
+    setError('');
   };
 
   const onChangePassword = (e) => {
     const value = e.target.value;
     setPassword2(value);
-    setError("");
+    setError('');
   };
 
   const inputEvent = (event) => {
@@ -67,49 +69,56 @@ const Login = () => {
   };
 
   const Eye = () => {
-    if (password == "password") {
-      setPassword("text");
+    if (password == 'password') {
+      setPassword('text');
       seteye(false);
       settype(true);
     } else {
-      setPassword("password");
+      setPassword('password');
       seteye(true);
       settype(false);
     }
   };
   return (
     <Fragment>
-      <div className="d-lg-flex half">
-        <div className="bg order-1" style={{ backgroundImage: 'url("/images/secondhand.png")' }} />
-        <div className="contents order-2 order-md-1">
-          <div className="container">
-            <div className="row align-items-center justify-content-center">
-              <div className="col-md-7">
-                <div className="mb-4">
-                  <h3 className="title-login">Masuk</h3>
+      <div className='d-lg-flex half'>
+        <div className='bg order-1' style={{ backgroundImage: 'url("/images/secondhand.png")' }} />
+        <div className='contents order-2 order-md-1'>
+          <div className='container'>
+            <div className='row align-items-center justify-content-center'>
+              <div className='col-md-7'>
+                <div className='mb-4'>
+                  <h3 className='title-login'>Masuk</h3>
                 </div>
                 <form>
                   {error && (
-                    <div className="alert alert-danger">
+                    <div className='alert alert-danger'>
                       <p>{error}</p>
                     </div>
                   )}
-                  <div className="form-group first">
+                  <div className='form-group first'>
                     <label>email</label>
-                    <input type="text" className="form-control" placeholder="Masukan Email" value={email} onChange={onChangeEmail} />
+                    <input type='text' className='form-control' placeholder='Masukan Email' value={email} onChange={onChangeEmail} />
                   </div>
-                  <div className="form-group last mt-5">
+                  <div className='form-group last mt-5'>
                     <label>Password</label>
-                    <input type={password} className="form-control" placeholder="Enter your password" name="password" value={password2} onChange={onChangePassword} />
-                    <i onClick={Eye} className={`bi ${eye ? "bi bi-eye-slash" : "bi bi-eye"}`}></i>
+                    <input type={password} className='form-control' placeholder='Enter your password' name='password' value={password2} onChange={onChangePassword} />
+                    <i onClick={Eye} className={`bi ${eye ? 'bi bi-eye-slash' : 'bi bi-eye'}`}></i>
                   </div>
-                  <button type="submit" onClick={submitLogin} className="btn btn-block">
-                    Masuk
+                  <button
+                    type='submit'
+                    onClick={(event) => {
+                      setLoading(true);
+                      submitLogin(event);
+                    }}
+                    className='btn btn-block'
+                  >
+                    {loading ? <>Loading...</> : <>Masuk</>}
                   </button>
-                  <div className="login-form mt-4 ms-5">
-                    <p className="text-center">
-                      Belum Punya Akun?{" "}
-                      <Link to="/register" className="link-already-login">
+                  <div className='login-form mt-4 ms-5'>
+                    <p className='text-center'>
+                      Belum Punya Akun?{' '}
+                      <Link to='/register' className='link-already-login'>
                         Daftar di sini
                       </Link>
                     </p>
